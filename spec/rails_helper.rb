237803d49b1cd2330a203de37d7feaea8363dd33
Chris Fung <aergonaut@gmail.com>
2018-07-28 17:44:34 -0700

    switch driven_by directive
    https://github.com/teamcapybara/capybara/issues/1860#issuecomment-328279083

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,7 +65,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
   end
 
   config.before(:each, type: :controller) do
